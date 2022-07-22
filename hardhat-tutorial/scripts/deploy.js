4ed93e6c9bbcdbcefda777f7d5d54a9098bfa338
georgediepiriye <georgediepiriye4u@gmail.com>
2022-07-22 10:11:41 +0100

    added deployment script and network config

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,29 +1,35 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { FIGHT_PUNKS_NFT_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Address of the Fight Punk NFT contract that was deployed previously
+  const fightPunksNFTContract = FIGHT_PUNKS_NFT_CONTRACT_ADDRESS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  /*
+    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+    so fghtPunksTokenContract here is a factory for instances of our FightPunkToken contract.
+    */
+  const fightPunksTokenContract = await ethers.getContractFactory(
+    "FightPunksToken"
+  );
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // deploy the contract
+  const deployedFightPunksTokenContract = await fightPunksTokenContract.deploy(
+    fightPunksNFTContract
+  );
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  // print the address of the deployed contract
+  console.log(
+    "Fight Punks Token Contract Address:",
+    deployedFightPunksTokenContract.address
+  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
